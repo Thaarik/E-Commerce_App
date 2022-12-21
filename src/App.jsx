68d76ac0a84edf593f84a94f2e03c32b2cb2eec3
Thaarik <a.thaarikahamed@gmail.com>
2022-12-21 22:52:23 +0000

    Created cart,login,register,product pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,11 @@
-import Home from "./pages/Home";
+// import Home from "./pages/Home";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import Product from "./pages/Product";
+// import ProductList from "./pages/ProductList";
 
 const App = () => {
-  return <Home/>
+  return <Cart/>
 };
 
 export default App;
---
+++ src/pages/Product.jsx
@@ -0,0 +1,155 @@
+import { Add, Remove } from "@material-ui/icons";
+import React from "react";
+import styled from "styled-components";
+import Announcement from "../components/Announcement";
+import Footer from "../components/Footer";
+import Navbar from "../components/Navbar";
+import Newsletter from "../components/Newsletter";
+import productImg from "./../assets/mediamodifier-7cERndkOyDw-unsplash.jpg";
+
+const Container = styled.div``;
+const Wrapper = styled.div`
+  padding: 50px;
+  display: flex;
+`;
+const ImgContainer = styled.div`
+  flex: 1;
+`;
+const Image = styled.img`
+  width: 100%;
+  height: 90vh;
+  object-fit: cover;
+`;
+const InfoContainer = styled.div`
+  flex: 1;
+  padding: 0 50px;
+`;
+const Title = styled.h1`
+  font-weight: 600;
+`;
+const Desc = styled.p`
+  margin: 20px 0;
+`;
+const Price = styled.span`
+  font-weight: 100;
+  font-size: 40px;
+`;
+const FilterContainer = styled.div`
+  width: 50%;
+  margin: 20px 0px;
+  display: flex;
+  justify-content: space-between;
+`;
+const Filter = styled.div`
+  display: flex;
+  align-items: center;
+`;
+const FilterTitle = styled.span`
+  font-size: 20px;
+  font-weight: 200;
+`;
+const FilterColor = styled.div`
+  width: 20px;
+  height: 20px;
+  border: 1px solid grey;
+  border-radius: 50%;
+  background-color: ${(props) => props.color};
+  margin: 0 5px;
+  cursor: pointer;
+`;
+const FilterSize = styled.select`
+  margin-left: 10px;
+  padding: 5px;
+`;
+const FilterSizeOption = styled.option``;
+const AddContainer = styled.div`
+  width: 50%;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+const AmountContainer = styled.div`
+  display: flex;
+  align-items: center;
+  font-weight: 700;
+`;
+const Amount = styled.span`
+  width: 30px;
+  height: 30px;
+  border-radius: 10px;
+  border: 1px solid teal;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 0px 5px;
+`;
+const Button = styled.button`
+  padding: 15px;
+  border: 2px solid teal;
+  border-radius:5px ;
+  background-color: white;
+  cursor: pointer;
+  font-weight: 500;
+  &:hover {
+    background-color: lightgrey;
+  }
+`;
+
+const Product = () => {
+  return (
+    <Container>
+      <Navbar />
+      <Announcement />
+      <Wrapper>
+        <ImgContainer>
+          <Image src={productImg} />
+        </ImgContainer>
+        <InfoContainer>
+          <Title>Sweatshirt</Title>
+          <Desc>
+            Deserunt ullamco ex aliqua excepteur pariatur irure mollit nostrud
+            aliqua duis labore dolor do. Eu Lorem cupidatat non reprehenderit ut
+            velit id do. Eiusmod enim voluptate aliqua mollit cupidatat
+            adipisicing exercitation minim minim sunt. Magna labore deserunt
+            nostrud et deserunt esse enim aliqua est id tempor ut officia. Anim
+            dolor deserunt aliquip exercitation non sit reprehenderit eu qui ad
+            nostrud veniam.
+          </Desc>
+          <Price>£ 30</Price>
+          <FilterContainer>
+            <Filter>
+              <FilterTitle>Color</FilterTitle>
+              <FilterColor color="black" />
+              <FilterColor color="white" />
+              <FilterColor color="blue" />
+              <FilterColor color="grey" />
+            </Filter>
+            <Filter>
+              <FilterTitle>Size</FilterTitle>
+              <FilterSize>
+                <FilterSizeOption>XS</FilterSizeOption>
+                <FilterSizeOption>S</FilterSizeOption>
+                <FilterSizeOption>M</FilterSizeOption>
+                <FilterSizeOption>L</FilterSizeOption>
+                <FilterSizeOption>XL</FilterSizeOption>
+                <FilterSizeOption>XXL</FilterSizeOption>
+              </FilterSize>
+            </Filter>
+          </FilterContainer>
+          <AddContainer>
+            <AmountContainer>
+              <Remove />
+              <Amount>1</Amount>
+              <Add />
+            </AmountContainer>
+            <Button>ADD TO CART</Button>
+          </AddContainer>
+        </InfoContainer>
+      </Wrapper>
+      <Newsletter />
+      <Footer />
+    </Container>
+  );
+};
+
+export default Product;
